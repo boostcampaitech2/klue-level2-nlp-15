2bf8d2c8c476b9f15ed7ad9e5c082984f0965c91
noopy <young_ahn@yonsei.ac.kr>
2021-10-10 23:47:57 +0900

    fix: RBERT

--- utils/metrics.py
+++ utils/metrics.py
@@ -76,3 +76,20 @@ def compute_metrics(keys, logitss):
     acc = accuracy_score(labels, preds)  # 리더보드 평가에는 포함되지 않습니다.
 
     return f1, auprc, acc
+
+
+class Metrics(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
