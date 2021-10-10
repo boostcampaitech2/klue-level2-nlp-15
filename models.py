085324e247a18de37185125a6d768d0966e93e24
noopy <young_ahn@yonsei.ac.kr>
2021-10-10 19:16:24 +0900

    update: generalize for other experimented models

--- models.py
+++ models.py
@@ -5,33 +5,36 @@ from transformers import AutoModel
 from utils.loss import FocalLoss
 
 
-
 class IBModel(nn.Module):
-    '''
+    """
     Model implementation of paper: 'An Improved Baseline for Sentence-level Relation Extraction'
     with some customizations added to match the performance on KLUE RE task
     https://arxiv.org/abs/2102.01373
-    '''
+    """
+
     def __init__(self, model_name, config):
         super().__init__()
         self.encoder = AutoModel.from_pretrained(model_name, config=config)
         hidden_size = config.hidden_size
-        self.loss_fnt = FocalLoss(gamma = 1.0)
+        self.loss_fnt = FocalLoss(gamma=1.0)
         self.classifier = nn.Sequential(
-            nn.Linear(hidden_size*2, hidden_size),
+            nn.Linear(hidden_size * 2, hidden_size),
             nn.ReLU(),
             nn.Dropout(p=0.1),
-            nn.Linear(hidden_size, config.num_labels)
+            nn.Linear(hidden_size, config.num_labels),
         )
 
     @autocast()
-    def forward(self, input_ids=None,
-                attention_mask=None,
-                labels=None,
-                ss=None,
-                se = None,
-                es=None,
-                ee = None,):
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        labels=None,
+        ss=None,
+        se=None,
+        es=None,
+        ee=None,
+    ):
         outputs = self.encoder(
             input_ids,
             attention_mask=attention_mask,
@@ -40,7 +43,7 @@ class IBModel(nn.Module):
         idx = torch.arange(input_ids.size(0)).to(input_ids.device)
         ss_emb = pooled_output[idx, ss]
         es_emb = pooled_output[idx, es]
-        h = torch.cat((ss_emb,es_emb), dim=-1)
+        h = torch.cat((ss_emb, es_emb), dim=-1)
         logits = self.classifier(h)
         outputs = (logits,)
         if labels is not None:
